de49f89ec5e0ddb67a02de10c348220651b3daf1
feifan <739767591@qq.com>
2018-07-10 18:41:24 +0800

    Springboot搭建

---
+++ ftree/src/main/java/com/files/ftree/FtreeApplication.java
@@ -0,0 +1,12 @@
+package com.files.ftree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtreeApplication.class, args);
+	}
+}
